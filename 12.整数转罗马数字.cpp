250ca5d434a10f47ac97e0b071ea3c4ebbf4b923
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-01-30 20:00:45 +0800

    is ok for java

---
+++ 12.整数转罗马数字.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file 12.整数转罗马数字.cpp
+ * @author Crpdim (crpdim7@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string reps[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        string res;
+        for(int i=0; i<13; i++){
+            while(num>=values[i]){
+                num -= values[i];
+                res += reps[i];
+            }
+        }
+        return res;
+    }
+};
---
+++ hello.py
@@ -0,0 +1,5 @@
+ans = 0;
+for i in range(10000):
+    ans += i
+    print("ans = ",ans,"i = ",i)
+print(ans)
