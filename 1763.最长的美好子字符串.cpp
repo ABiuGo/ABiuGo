fd2dfeec45ebaed4a0d687e2c91df3492cf8efe4
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-02-03 16:03:57 +0800

    Create 1763.最长的美好子字符串.cpp

---
+++ 1763.最长的美好子字符串.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file 1763.最长的美好子字符串.cpp
+ * @author Crpdim (crpdim7@gmail.com)
+ * @brief 当一个字符串 s 包含的每一种字母的大写和小写形式 同时 出现在 s 中，就称这个字符串 s 是 美好 字符串。比方说，"abABB" 是美好字符串，因为 'A' 和 'a' 同时出现了，且 'B' 和 'b' 也同时出现了。然而，"abA" 不是美好字符串因为 'b' 出现了，而 'B' 没有出现。
+ * 给你一个字符串 s ，请你返回 s 最长的 美好子字符串 。如果有多个答案，请你返回 最早 出现的一个。如果不存在美好子字符串，请你返回一个空字符串。
+ * @version 0.1
+ * @date 2022-02-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        int n = s.size();
+        int maxPos = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; ++i) {
+            int lower = 0;
+            int upper = 0;
+            for (int j = i; j < n; ++j) {
+                if (islower(s[j])) {
+                    lower |= 1 << (s[j] - 'a');
+                } else {
+                    upper |= 1 << (s[j] - 'A');
+                }
+                if (lower == upper && j - i + 1 > maxLen) {
+                    maxPos = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+        return s.substr(maxPos, maxLen);
+    }
+};
