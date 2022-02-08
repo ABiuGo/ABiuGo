b09a46ad276eb18cfcbddc42ef3f90992e40f62f
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-02-08 17:08:47 +0800

    Update 100.相同的树.cpp

--- 100.相同的树.cpp
+++ 100.相同的树.cpp
@@ -1,7 +1,14 @@
-
-
-
-
+/**
+ * @file 100.相同的树.cpp
+ * @author Crpdim (crpdim7@gmail.com)
+ * @brief 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * @version 0.1
+ * @date 2022-02-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 /**
  * Definition for a binary tree node. 
