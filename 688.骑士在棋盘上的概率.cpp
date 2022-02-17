e206c82917225bbff8302e8a7cc806409cd2a50c
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-02-17 21:05:45 +0800

    688

---
+++ 688.骑士在棋盘上的概率.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************
+> File Name:     688.骑士在棋盘上的概率.cpp
+> Author:        Crpdim
+> mail:         crpdim7@gmail.com
+> Created Time:  四  2/17 21:05:16 2022
+> Description:   
+ ************************************************************************/
+
+class Solution {
+public:
+    vector<vector<int>> dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(k + 1, vector<vector<double>>(n, vector<double>(n)));
+        for (int step = 0; step <= k; step++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (step == 0) {
+                        dp[step][i][j] = 1;
+                    } else {
+                        for (auto & dir : dirs) {
+                            int ni = i + dir[0], nj = j + dir[1];
+                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                                dp[step][i][j] += dp[step - 1][ni][nj] / 8;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dp[k][row][column];
+    }
+};
