e8b13fd119a6657bef4e64481eba49f6df59abd6
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-02-23 12:07:19 +0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************
+> File Name:     hello.cpp
+> Author:        Crpdim
+> mail:         crpdim7@gmail.com
+> Created Time:  三  2/23 02:34:35 2022
+> Description:   
+ ************************************************************************/
+#include<iostream>
+
+using namespace std;
+
+
+class A {
+    
+    public int a = 10;
+    A() {
+        a = 100;
+    }
+    public void ans() {
+        ++a;
+    }
+};
+int prate(int a, int b, int ) {
+    cout << "hello";
+    return a;
+}
+
+int main() {
+    A aa = new A();
+   
+}
