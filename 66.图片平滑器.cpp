2727acbc1197b40bb8db31840e20a154064fa71f
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-03-24 23:20:16 +0800

    Create 66.图片平滑器.cpp

---
+++ 66.图片平滑器.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file 66.图片平滑器.cpp
+ * @author Crpdim (crpdim7@gmail.com)
+ * @brief   https://leetcode-cn.com/problems/image-smoother/
+ * @version 0.1
+ * @date 2022-03-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size(), n = img[0].size();
+        vector<vector<int>> ret(m, vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = 0, sum = 0;
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        if (x >= 0 && x < m && y >= 0 && y < n) {
+                            num++;
+                            sum += img[x][y];
+                        }
+                    }
+                }
+                ret[i][j] = sum / num;
+            }
+        }
+        return ret;
+    }
+};
